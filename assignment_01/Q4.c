7b49bac4eccd9e6b1f4c3c4ba73546e588a630cc
heeyshubham <55744455+heeyshubham@users.noreply.github.com>
2022-09-14 05:01:08 +0530

    Add files via upload

---
+++ assignment_01/Q4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{   
+    //Q4
+
+    double r;
+    printf("Enter Radius of the Circle = ");
+    scanf("%lf",&r);
+    printf("\nArea of the circle with radius %lf is %lf " ,r,3.14*r*r);
+    
+    return 0;
+    
+    //Q3 SOLN END
+
+}
