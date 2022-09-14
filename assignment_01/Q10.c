27a06e42d6443ae997ff5d8377e3a2e7cc51a51b
heeyshubham <55744455+heeyshubham@users.noreply.github.com>
2022-09-14 06:49:23 +0530

    Add files via upload

---
+++ assignment_01/Q10.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{   
+    //Q10
+
+    char ddmmyyyy[11];
+    printf("Enter date (dd/mm/yyyy): ");
+    fgets(ddmmyyyy, 11, stdin);
+    printf("Day - %.2s Month - %.*s Year - %.*s", ddmmyyyy, 2, ddmmyyyy + 3, 4, ddmmyyyy + 6);
+    
+    //Q10 SOLN END
+
+}
