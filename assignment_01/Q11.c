ba8900da1448b733a994c75cfaf8cd34a5c21ace
heeyshubham <55744455+heeyshubham@users.noreply.github.com>
2022-09-14 07:01:50 +0530

    Add files via upload

---
+++ assignment_01/Q11.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{   
+    //Q11
+
+    char hhmm[6];
+    printf("Enter Time (HH:MM): ");
+    fgets(hhmm, 6, stdin);
+    printf("%.2s Hour and %.*s Minute", hhmm, 2, hhmm + 3);
+    
+    //Q11 SOLN END
+
+}
